d1dbae69451a76218bf70012e15642c0cf2b7f83
HeesungLee <dlgmltjd0904@gmail.com>
2022-10-13 13:40:43 +0900

    raspberrypi

---
+++ opencv/02. img_save.py
@@ -0,0 +1,8 @@
+import cv2
+
+imageFile = 'jenny.jpg'
+img = cv2.imread(imageFile) # cv2.imread(imageFile, cv2.IMREAD_COLOR)
+cv2.imwrite('jenny.bmp', img) #jenny.bmp로 저장
+cv2.imwrite('jenny.png', img) #jenny.png로 저장
+cv2.imwrite('jenny2.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, 90]) #압축률 90의 JPEG 영상으로 저장, 압축률 범위는 0~100 , 기본값은 95
+cv2.imwrite('jenny2.png', img, [cv2.IMWRITE_PNG_COMPRESSION, 9]) #압축률 9의 PNG 영상으로 저장, 압축률 범위는 0~9, 기본값은 3
---
+++ opencv/03. img_matplotlib01.py
@@ -0,0 +1,12 @@
+import cv2
+from matplotlib import pyplot as plt
+
+imageFile = 'jenny.jpg'
+imgBGR = cv2.imread(imageFile)
+plt.axis('off')
+#plt.imshow(imgBGR)
+#plt.show()
+
+imgRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
+plt.imshow()
+plt.show()
