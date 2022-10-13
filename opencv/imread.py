e04de1a520d1521458570b8f975604cdf5433802
HeesungLee <dlgmltjd0904@gmail.com>
2022-10-13 11:21:59 +0900

    raspberrypi

---
+++ opencv/imread.py
@@ -0,0 +1,10 @@
+import cv2
+
+imageFile = 'jenny.jpg'
+img = cv2.imread(imageFile)
+img_gray = cv2.imread(imageFile, 0)
+cv2.imshow('Jenny colour', img)
+cv2.imshow('Jenny Gray', img_gray)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
